da0e5ddc2424dc2159c3f53cfd60671bf402488d
Anshul <anshul@wealthy.in>
2024-11-16 14:02:57 +0530

    add some config

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -2,20 +2,34 @@
 require("nvchad.configs.lspconfig").defaults()
 
 local lspconfig = require "lspconfig"
-
 -- EXAMPLE
-local servers = { "html", "cssls" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
+-- local servers = { "html", "cssls", "clangd", "ruff", "basedpyright", "gopls" }
 -- lsps with default config
-for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = nvlsp.on_attach,
-    on_init = nvlsp.on_init,
-    capabilities = nvlsp.capabilities,
-  }
-end
+-- for _, lsp in ipairs(servers) do
+--   lspconfig[lsp].setup {
+--     on_attach = nvlsp.on_attach,
+--     on_init = nvlsp.on_init,
+--     capabilities = nvlsp.capabilities,
+  -- }
+-- end
 
+local servers = {
+  html = {},
+  cssls = {},
+  clangd = {},
+  ruff = {},
+  basedpyright = {},
+  gopls = {},
+  sqlls = {}
+}
+for name, opts in pairs(servers) do
+  opts.on_attach = nvlsp.on_attach
+  opts.on_init = nvlsp.on_init
+  opts.capabilities = nvlsp.capabilities
+  lspconfig[name].setup(opts)
+end
 -- configuring single server, example: typescript
 -- lspconfig.ts_ls.setup {
 --   on_attach = nvlsp.on_attach,
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -12,7 +12,20 @@ return {
       require "configs.lspconfig"
     end,
   },
-
+{
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+        require("nvim-surround").setup({
+            -- Configuration here, or leave empty to use defaults
+        })
+    end
+},
+  {
+"tpope/vim-fugitive",
+cmd = "Git"
+}
   -- {
   -- 	"nvim-treesitter/nvim-treesitter",
   -- 	opts = {
